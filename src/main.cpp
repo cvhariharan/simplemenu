829406b7c8cb1cee933e90098fdcff9387029246
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2023-04-12 09:18:27 +0530

    Use HW SPI

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,16 @@
 #include <WelcomePage.h>
 #include <ListPage.h>
 #include <Button.h>
+#include <Encoder.h>
 
 ListPage listPage(NULL, NULL, 0, "listpage");
 WelcomePage welcome(NULL, (Page *[]){&listPage}, 1, "welcomepage");
 Page *currentPage;
 
-U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 14, /* data=*/ 13, /* CS=*/ 2, /* reset=*/ U8X8_PIN_NONE);
-
+// U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 14, /* data=*/ 13, /* CS=*/ 2, /* reset=*/ U8X8_PIN_NONE);
+U8G2_ST7920_128X64_F_HW_SPI u8g2(U8G2_R0, 2);
 Button button(4);
+Encoder encoder(14,12);
 
 void setup() {
     u8g2.begin();
@@ -20,11 +22,16 @@ void setup() {
 }
 
 void loop() {
-    int clicks = button.update();  
-    u8g2.firstPage();      
+    int start = millis();
+    int clicks = button.update();
+
+    u8g2.firstPage();
     do {
         currentPage->draw(u8g2);
-    } while( u8g2.nextPage() );
+    } while(u8g2.nextPage());
+    
+    int end = millis();
+    Serial.println(end-start);
 
     if(clicks > 0) {
         currentPage = currentPage->update(clicks);
